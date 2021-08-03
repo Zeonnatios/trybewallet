0e50e91ff350b3744e9da466600298da96cbcb0e
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-08-03 13:57:03 -0300

    Updates

---
+++ precache-manifest.e38578096bfb0ff9ada0640b6f44e88d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a569cae80e1bc85cde251cea7401a49",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "f5189ceb83a95f2cab83",
+    "url": "/trybewallet/static/css/main.4e714fc4.chunk.css"
+  },
+  {
+    "revision": "bcc681e8e9662f11386a",
+    "url": "/trybewallet/static/js/2.71217d02.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.71217d02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5189ceb83a95f2cab83",
+    "url": "/trybewallet/static/js/main.1d241a9b.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  }
+]);
